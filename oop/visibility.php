9498fac2aa5014cf7cb4a15b18eb2c0ca9f499a1
Rizal Fahla <75971110+rizalfahla@users.noreply.github.com>
2024-10-08 16:04:18 +0700

    Video 9 - Visibility

---
+++ oop/visibility.php
@@ -0,0 +1,98 @@
+<?php
+// Jualan Produk
+// Komik
+// Game
+
+class Produk
+{
+  public $judul, $penulis, $penerbit;
+
+  protected $diskon = 0;
+
+  private $harga;
+
+  public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0)
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
+
+  public function getHarga()
+  {
+    return $this->harga - ($this->harga * $this->diskon / 100);
+  }
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->getHarga()})";
+    return $str;
+  }
+}
+
+class Komik extends Produk
+{
+  public $jmlHalaman;
+
+  public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0)
+  {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+    $this->jmlHalaman = $jmlHalaman;
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
+    return $str;
+  }
+}
+
+class Game extends Produk
+{
+  public $waktuMain;
+
+  public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $waktuMain = 0)
+  {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+    $this->waktuMain = $waktuMain;
+  }
+
+  public function setDiskon($diskon)
+  {
+    $this->diskon = $diskon;
+  }
+
+  public function getInfoProduk()
+  {
+    $str = "Game : " . parent::getInfoProduk() . " - {$this->waktuMain} Jam.";
+    return $str;
+  }
+}
+
+class CetakInfoProduk
+{
+  public function cetak(Produk $produk)
+  {
+    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+    return $str;
+  }
+}
+
+$produk3 = new Komik("Penjelajahan Riung Bandung", "Daffa", "Tomang", 200000, 100);
+$produk4 = new Game("Mozra", "Uloh", "Amidis", 300000, 50);
+
+echo $produk3->getInfoProduk();
+echo "<br>";
+echo $produk4->getInfoProduk();
+
+
+echo "<hr>";
+
+$produk4->setDiskon(50);
+echo $produk4->getHarga();
