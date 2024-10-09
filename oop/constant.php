11cb4f1e209baaf1bac3a217f98daf29a235f88d
Rizal Fahla <75971110+rizalfahla@users.noreply.github.com>
2024-10-09 22:07:17 +0700

    Video - 12 Constant

---
+++ oop/constant.php
@@ -0,0 +1,16 @@
+<?php 
+// define ('NAMA', 'Rizal Fahla');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 32;
+// echo UMUR;
+
+class Coba {
+    const NAMA = 'Rizal Fahla';
+}
+
+echo Coba::NAMA;
+
+?>
