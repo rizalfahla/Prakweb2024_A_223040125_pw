fdf6bcfb698023a4628cbea004a63e7e5da45ea0
Rizal Fahla <75971110+rizalfahla@users.noreply.github.com>
2024-10-15 16:04:34 +0700

    membereskan video 8

--- mvc/app/models/Mahasiswa_model.php
+++ mvc/app/models/Mahasiswa_model.php
@@ -1,26 +1,23 @@
 <?php 
 
 class Mahasiswa_model {
-        
-        private $dbh;//database handler
-        private $stmt;
+    
+    private $table = 'mahasiswa';
+    private $db;
 
-        public function __construct()
-        {
-            //data source name
-            $dsn = 'mysql:host=localhost;dbname=phpmvc';
+    public function __construct(){
+        $this->db = new Database;
+    }
 
-            try {
-                $this -> dbh = new PDO ($dsn, 'root', '');
-            } catch (PDOException $e) {
-                die($e->getMessage());
-            }
-        }
+    public function getAllMahasiswa(){
+        // Menggunakan objek $this->db untuk query
+        $this->db->query("SELECT * FROM $this->table");
+        return $this->db->resultSet();
+    }
 
-        public function getAllMahasiswa()
-        {
-            $this->stmt = $this-> dbh-> prepare('SELECT * FROM mahasiswa');
-            $this->stmt->execute();
-            return $this->stmt->fetchAll(PDO ::FETCH_ASSOC);
-        }
+    public function getMahasiswaById($id){
+        $this -> db -> query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
 }
